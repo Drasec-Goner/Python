b5ef92342a576ded801fd4555e25e87ac3dd8a55
Soumya Manna <soumyamanna04@gmail.com>
2022-10-01 16:24:03 +0530

    Python

---
+++ swap.py
@@ -0,0 +1,10 @@
+#Take inputs from user for two numbers(x and y)
+
+firstN=input("Enter the first Number: ")
+secondN=input("Enter the second Number: ")
+temp = int(firstN)
+firstN = int(secondN)
+secondN = int(temp)
+
+print("The First number swapped: ", firstN)
+print("The Second number swapped: ", secondN)
