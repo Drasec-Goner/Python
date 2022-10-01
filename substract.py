09220a2145c71b38436016a019f37aa84b15c3b0
Soumya Manna <soumyamanna04@gmail.com>
2022-10-01 16:08:24 +0530

    Python

--- substract.py
+++ substract.py
@@ -1,9 +1,9 @@
 # Create two variables (x and y) and assign values to them
-# Create a third variable(sum) and add x and y
-# print the sum
+# Create a third variable(sum) and substract x and y
+# print the substraction
 
 firstNumber=input("Enter the First Number")
 secondNumber=input("Enter the Second Number") 
 
-sum = int(firstNumber) + int(secondNumber)
-print(sum)  
+substract = int(firstNumber) - int(secondNumber)
+print(substract)  
