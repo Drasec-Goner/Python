0e99f43b77e5efb97ee445604055baa19d859870
Soumya Manna <soumyamanna04@gmail.com>
2022-10-03 23:22:14 +0530

    Python

---
+++ areaTriangle.py
@@ -0,0 +1,11 @@
+#program to find the square root of a traingle
+
+a = float(input('Enter the length of first side: '))  
+b = float(input('Enter the length of second side: '))  
+c = float(input('Enter the length of final side: '))
+
+s =(a+b+c)/2
+
+area = float((s*(s-a)(s-b)(s-c)) ** 0.5)
+
+print("The Area of the triangle is: ", area)
---
+++ squareRoot.py
@@ -0,0 +1,6 @@
+#To find the square root of a number
+
+num = input("Enter the number: ")
+sqrt = int(num) ** 0.5
+
+print(sqrt)
