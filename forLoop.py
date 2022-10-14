3e2e5432bbc190ca46768085c6d53b74ee4eac0d
Soumya Manna <soumyamanna04@gmail.com>
2022-10-14 13:38:46 +0530

    Python

---
+++ forLoop.py
@@ -0,0 +1,7 @@
+#program to use for loop to print the input from user till n times
+
+sen = input("Enter the word: ")
+n = int(input("Enter the number of times it will be repeated: "))
+
+for i in range(n):
+    print(sen)
---
+++ whileLoop.py
@@ -0,0 +1,7 @@
+#program to use while loop to print input from user 10 times
+
+a = input("Enter a sentence: ")
+i=1
+while i <= 10:
+    print(a)
+    i+=1
