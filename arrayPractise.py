0ec07fab9bcd7c195273d5d636ac99bd064d167d
Soumya Manna <soumyamanna04@gmail.com>
2022-10-16 13:49:38 +0530

    Python

---
+++ arrayPractise.py
@@ -0,0 +1,12 @@
+a = "Hello World"
+b = "Soumya Manna"
+print(a[6])
+print(f"{b[7]}{b[5]}{b[9]}")
+print(f"{b[0:4]}{b[1]}")
+print(len(a))
+print(len(b))
+
+if "Man" in b:
+    print("Yes! It is there.")
+else:
+    print("No!")
