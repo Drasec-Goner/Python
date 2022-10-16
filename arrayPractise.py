ec11932d795a73e3db9cd6b7af64e5c01bb3e049
Soumya Manna <soumyamanna04@gmail.com>
2022-10-16 15:29:32 +0530

    Python

--- arrayPractise.py
+++ arrayPractise.py
@@ -1,8 +1,13 @@
 a = "Hello World"
 b = "Soumya Manna"
+sentence = "It's a me Mario"
 print(a[6])
-print(f"{b[7]}{b[5]}{b[9]}")
+print(f"{b[7]}{b[5]}{b[9]}{b[9]}{b[8]}")
 print(f"{b[0:4]}{b[1]}")
+print(sentence[:])
+print(sentence[-5:])
+print(a[-5:-2])
+print(sentence[:7])
 print(len(a))
 print(len(b))
 
---
+++ lastDigit.py
@@ -0,0 +1,6 @@
+num = int(input("Enter the Number: "))
+ld=num%10
+if ld%3 == 0:
+    print("Yes")
+else:
+    print("No")
